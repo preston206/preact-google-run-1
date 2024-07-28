d2e8819c8d12dfe5a6b2593e5481393a03102a19
preston <broberts9@gmail.com>
2024-07-27 23:54:23 -0700

    initializing project

---
+++ server/index.js
@@ -0,0 +1,19 @@
+const Koa = require('koa');
+const Router = require('koa-router');
+const static = require('koa-static');
+const path = require('path');
+
+const server = new Koa();
+const router = new Router();
+
+// Serve static files from the build directory
+server.use(static(path.join(__dirname, '../client/dist')));
+
+// Your API routes would go here
+router.get('/api/data', async (ctx) => {
+  ctx.body = { message: 'Hello from Koa!' };
+});
+
+server.use(router.routes());
+
+module.exports = server;
